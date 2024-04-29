29805da7f8cd5ebaa350de6e82b1b716ce789c19
ANKIT3964 <113796066+ANKIT3964@users.noreply.github.com>
2024-04-29 13:24:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ STREAM_MODE = is_enabled((environ.get('STREAM_MODE', "false")), False)
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1991522624 1197918807').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001954637243').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001954637243 -1001519696446 -1001736667926 -1001954637243').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
